78f05488ea8e195ae549dd352f613a9b0d65ab8b
Esteban <eegnom1807@gmail.com>
2026-01-29 22:57:06 -0600

    Permit fk on SQL lite

--- app/db.py
+++ app/db.py
@@ -1,5 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+import sqlite3
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+
+@event.listens_for(Engine, "connect")
+def enable_sqlite_foreign_keys(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
